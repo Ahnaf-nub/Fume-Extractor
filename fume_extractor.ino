335c740d46938d7e4228d5d885b344a75a45dd58
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-07-24 21:49:56 +0600

    Add files via upload

---
+++ fume_extractor.ino
@@ -0,0 +1,31 @@
+#include <ESP32Servo.h>
+#include <MQ135.h>
+
+#define relay 8
+int pos = 0;
+ESP32Servo servo;
+
+void setup() {
+  servo.attach(10);
+  pinMode(relay, OUTPUT);
+}
+
+void loop() {
+  if (ppm > 500) {
+    digitalWrite(relay, HIGH);
+    sweep();
+  } else {
+    digitalWrite(relay, LOW);
+  }
+}
+
+void sweep {
+  for (pos = 0; pos <= 180; pos += 1) {
+    myservo.write(pos);
+    delay(10);
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    delay(10);
+  }
+}
